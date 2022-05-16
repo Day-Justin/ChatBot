05451487876ff2db67651ae7dd5d7b7d2355e761
JD <dayjustin.jd@gmail.com>
2022-05-16 14:12:15 -0400

    Adding a logger and checking functionalities of git

--- navi.py
+++ navi.py
@@ -2,9 +2,17 @@
 
 import discord
 imoport os
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logging.basicConfig(level = logging.INFO)
+logging = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename = 'discord.log', encoding = 'utf-8', mode = 'w')
+handler.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(name)s: %(message)s')
+logger.addHandler(handler)
 
 intents = discord.Intents.all()
 
